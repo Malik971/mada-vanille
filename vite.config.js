44843a80f1fef85ab05a7d64b767633e348db326
malik ibo <malik97un@gmail.com>
2024-11-11 11:40:56 +0100

    Déploiement sur Netfily

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/madagascar-vanilla/',  // Assure-toi que c'est le nom du repo
-  // autres configurations
+  base: '',  
 })
